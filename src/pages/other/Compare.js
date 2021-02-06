fb017bf5dfc66cfeeb11bfb1ad23bfb83c02fb90
Elijahscriptdev <elijahobominuru@gmail.com>
2021-02-06 20:36:29 +0100

    set up api integration for login and registration with test api

--- src/pages/other/Compare.js
+++ src/pages/other/Compare.js
@@ -18,7 +18,7 @@ const Compare = ({
   compareItems,
   addToCart,
   deleteFromCompare,
-  currency
+  currency,
 }) => {
   const { pathname } = location;
   const { addToast } = useToasts();
@@ -28,41 +28,41 @@ const Compare = ({
       <MetaTags>
         <title>Flone | Compare</title>
         <meta
-          name="description"
-          content="Compare page of flone react minimalist eCommerce template."
+          name='description'
+          content='Compare page of flone react minimalist eCommerce template.'
         />
       </MetaTags>
       <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>Home</BreadcrumbsItem>
       <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
         Compare
       </BreadcrumbsItem>
-      <LayoutOne headerTop="visible">
+      <LayoutOne headerTop='visible'>
         {/* breadcrumb */}
         <Breadcrumb />
-        <div className="compare-main-area pt-90 pb-100">
-          <div className="container">
+        <div className='compare-main-area pt-90 pb-100'>
+          <div className='container'>
             {compareItems && compareItems.length >= 1 ? (
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="compare-page-content">
-                    <div className="compare-table table-responsive">
-                      <table className="table table-bordered mb-0">
+              <div className='row'>
+                <div className='col-lg-12'>
+                  <div className='compare-page-content'>
+                    <div className='compare-table table-responsive'>
+                      <table className='table table-bordered mb-0'>
                         <tbody>
                           <tr>
-                            <th className="title-column">Product Info</th>
+                            <th className='title-column'>Product Info</th>
                             {compareItems.map((compareItem, key) => {
                               const cartItem = cartItems.filter(
-                                item => item.id === compareItem.id
+                                (item) => item.id === compareItem.id
                               )[0];
                               return (
-                                <td className="product-image-title" key={key}>
-                                  <div className="compare-remove">
+                                <td className='product-image-title' key={key}>
+                                  <div className='compare-remove'>
                                     <button
                                       onClick={() =>
                                         deleteFromCompare(compareItem, addToast)
                                       }
                                     >
-                                      <i className="pe-7s-trash" />
+                                      <i className='pe-7s-trash' />
                                     </button>
                                   </div>
                                   <Link
@@ -71,18 +71,18 @@ const Compare = ({
                                       "/product/" +
                                       compareItem.id
                                     }
-                                    className="image"
+                                    className='image'
                                   >
                                     <img
-                                      className="img-fluid"
+                                      className='img-fluid'
                                       src={
                                         process.env.PUBLIC_URL +
                                         compareItem.image[0]
                                       }
-                                      alt=""
+                                      alt=''
                                     />
                                   </Link>
-                                  <div className="product-title">
+                                  <div className='product-title'>
                                     <Link
                                       to={
                                         process.env.PUBLIC_URL +
@@ -93,12 +93,12 @@ const Compare = ({
                                       {compareItem.name}
                                     </Link>
                                   </div>
-                                  <div className="compare-btn">
+                                  <div className='compare-btn'>
                                     {compareItem.affiliateLink ? (
                                       <a
                                         href={compareItem.affiliateLink}
-                                        rel="noopener noreferrer"
-                                        target="_blank"
+                                        rel='noopener noreferrer'
+                                        target='_blank'
                                       >
                                         {" "}
                                         Buy now{" "}
@@ -138,7 +138,7 @@ const Compare = ({
                                           : "Add to cart"}
                                       </button>
                                     ) : (
-                                      <button disabled className="active">
+                                      <button disabled className='active'>
                                         Out of Stock
                                       </button>
                                     )}
@@ -148,7 +148,7 @@ const Compare = ({
                             })}
                           </tr>
                           <tr>
-                            <th className="title-column">Price</th>
+                            <th className='title-column'>Price</th>
                             {compareItems.map((compareItem, key) => {
                               const discountedPrice = getDiscountPrice(
                                 compareItem.price,
@@ -161,20 +161,20 @@ const Compare = ({
                                 discountedPrice * currency.currencyRate
                               ).toFixed(2);
                               return (
-                                <td className="product-price" key={key}>
+                                <td className='product-price' key={key}>
                                   {discountedPrice !== null ? (
                                     <Fragment>
-                                      <span className="amount old">
+                                      <span className='amount old'>
                                         {currency.currencySymbol +
                                           finalProductPrice}
                                       </span>
-                                      <span className="amount">
+                                      <span className='amount'>
                                         {currency.currencySymbol +
                                           finalDiscountedPrice}
                                       </span>
                                     </Fragment>
                                   ) : (
-                                    <span className="amount">
+                                    <span className='amount'>
                                       {currency.currencySymbol +
                                         finalProductPrice}
                                     </span>
@@ -185,10 +185,10 @@ const Compare = ({
                           </tr>
 
                           <tr>
-                            <th className="title-column">Description</th>
+                            <th className='title-column'>Description</th>
                             {compareItems.map((compareItem, key) => {
                               return (
-                                <td className="product-desc" key={key}>
+                                <td className='product-desc' key={key}>
                                   <p>
                                     {compareItem.shortDescription
                                       ? compareItem.shortDescription
@@ -200,10 +200,10 @@ const Compare = ({
                           </tr>
 
                           <tr>
-                            <th className="title-column">Rating</th>
+                            <th className='title-column'>Rating</th>
                             {compareItems.map((compareItem, key) => {
                               return (
-                                <td className="product-rating" key={key}>
+                                <td className='product-rating' key={key}>
                                   <Rating ratingValue={compareItem.rating} />
                                 </td>
                               );
@@ -216,13 +216,13 @@ const Compare = ({
                 </div>
               </div>
             ) : (
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="item-empty-area text-center">
-                    <div className="item-empty-area__icon mb-30">
-                      <i className="pe-7s-shuffle"></i>
+              <div className='row'>
+                <div className='col-lg-12'>
+                  <div className='item-empty-area text-center'>
+                    <div className='item-empty-area__icon mb-30'>
+                      <i className='pe-7s-shuffle'></i>
                     </div>
-                    <div className="item-empty-area__text">
+                    <div className='item-empty-area__text'>
                       No items found in compare <br />{" "}
                       <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
                         Add Items
@@ -245,18 +245,18 @@ Compare.propTypes = {
   compareItems: PropTypes.array,
   currency: PropTypes.object,
   location: PropTypes.object,
-  deleteFromCompare: PropTypes.func
+  deleteFromCompare: PropTypes.func,
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     cartItems: state.cartData,
     compareItems: state.compareData,
-    currency: state.currencyData
+    currency: state.currencyData,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     addToCart: (item, addToast, quantityCount) => {
       dispatch(addToCart(item, addToast, quantityCount));
@@ -264,7 +264,7 @@ const mapDispatchToProps = dispatch => {
 
     deleteFromCompare: (item, addToast) => {
       dispatch(deleteFromCompare(item, addToast));
-    }
+    },
   };
 };
 
--- src/pages/other/LoginRegister.js
+++ src/pages/other/LoginRegister.js
@@ -1,74 +1,126 @@
 import PropTypes from "prop-types";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 import MetaTags from "react-meta-tags";
-import { Link } from "react-router-dom";
 import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import Tab from "react-bootstrap/Tab";
 import Nav from "react-bootstrap/Nav";
 import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
+import { login, register } from "../../redux/actions/auth";
 
-const LoginRegister = ({ location }) => {
+const LoginRegister = ({ location, register, login, isAuthenticated }) => {
   const { pathname } = location;
+
+  // Register
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { first_name, last_name, email, password, password2 } = formData;
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+    console.log(formData);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    register({ first_name, last_name, email, password });
+    console.log(formData);
+    console.log("form submitted");
+  };
+
+  // login
+
+  const handleChange = (e) =>
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login({ email, password });
+  };
+
+  // redirect if login
+  if (isAuthenticated) {
+    return <Redirect to='/' />;
+  }
 
   return (
     <Fragment>
       <MetaTags>
         <title>Flone | Login</title>
         <meta
-          name="description"
-          content="Compare page of flone react minimalist eCommerce template."
+          name='description'
+          content='Compare page of flone react minimalist eCommerce template.'
         />
       </MetaTags>
       <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>Home</BreadcrumbsItem>
       <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
         Login Register
       </BreadcrumbsItem>
-      <LayoutOne headerTop="visible">
+      <LayoutOne headerTop='visible'>
         {/* breadcrumb */}
         <Breadcrumb />
-        <div className="login-register-area pt-100 pb-100">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-7 col-md-12 ml-auto mr-auto">
-                <div className="login-register-wrapper">
-                  <Tab.Container defaultActiveKey="login">
-                    <Nav variant="pills" className="login-register-tab-list">
+        <div className='login-register-area pt-100 pb-100'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-lg-7 col-md-12 ml-auto mr-auto'>
+                <div className='login-register-wrapper'>
+                  <Tab.Container defaultActiveKey='login'>
+                    <Nav variant='pills' className='login-register-tab-list'>
                       <Nav.Item>
-                        <Nav.Link eventKey="login">
+                        <Nav.Link eventKey='login'>
                           <h4>Login</h4>
                         </Nav.Link>
                       </Nav.Item>
                       <Nav.Item>
-                        <Nav.Link eventKey="register">
+                        <Nav.Link eventKey='register'>
                           <h4>Register</h4>
                         </Nav.Link>
                       </Nav.Item>
                     </Nav>
                     <Tab.Content>
-                      <Tab.Pane eventKey="login">
-                        <div className="login-form-container">
-                          <div className="login-register-form">
-                            <form>
+                      <Tab.Pane eventKey='login'>
+                        <div className='login-form-container'>
+                          <div className='login-register-form'>
+                            <form onSubmit={(e) => handleSubmit(e)}>
                               <input
-                                type="text"
-                                name="user-name"
-                                placeholder="Username"
+                                type='email'
+                                value={email}
+                                placeholder='eee@gmail.com'
+                                name='email'
+                                onChange={(e) => handleChange(e)}
+                                // required
                               />
                               <input
-                                type="password"
-                                name="user-password"
-                                placeholder="Password"
+                                type='password'
+                                placeholder='************'
+                                value={password}
+                                name='password'
+                                onChange={(e) => handleChange(e)}
+                                // minLength='6'
                               />
-                              <div className="button-box">
-                                <div className="login-toggle-btn">
-                                  <input type="checkbox" />
-                                  <label className="ml-10">Remember me</label>
+                              <div className='button-box'>
+                                <div className='login-toggle-btn'>
+                                  <input type='checkbox' />
+                                  <label className='ml-10'>Remember me</label>
                                   <Link to={process.env.PUBLIC_URL + "/"}>
                                     Forgot Password?
                                   </Link>
                                 </div>
-                                <button type="submit">
+                                <button type='submit'>
                                   <span>Login</span>
                                 </button>
                               </div>
@@ -76,27 +128,55 @@ const LoginRegister = ({ location }) => {
                           </div>
                         </div>
                       </Tab.Pane>
-                      <Tab.Pane eventKey="register">
-                        <div className="login-form-container">
-                          <div className="login-register-form">
-                            <form>
+                      <Tab.Pane eventKey='register'>
+                        <div className='login-form-container'>
+                          <div className='login-register-form'>
+                            <form onSubmit={(e) => onSubmit(e)}>
                               <input
-                                type="text"
-                                name="user-name"
-                                placeholder="Username"
+                                className='p'
+                                type='text'
+                                value={first_name}
+                                placeholder='First Name'
+                                name='first_name'
+                                onChange={(e) => onChange(e)}
+                                // required
                               />
                               <input
-                                type="password"
-                                name="user-password"
-                                placeholder="Password"
+                                className='p'
+                                type='text'
+                                value={last_name}
+                                placeholder='Last Name'
+                                name='last_name'
+                                onChange={(e) => onChange(e)}
+                                // required
                               />
                               <input
-                                name="user-email"
-                                placeholder="Email"
-                                type="email"
+                                type='email'
+                                value={email}
+                                placeholder='eee@gmail.com'
+                                name='email'
+                                onChange={(e) => onChange(e)}
+                                // required
                               />
-                              <div className="button-box">
-                                <button type="submit">
+                              <input
+                                type='password'
+                                placeholder='************'
+                                value={password}
+                                name='password'
+                                onChange={(e) => onChange(e)}
+                                // minLength='6'
+                              />
+                              <input
+                                type='password'
+                                placeholder='************'
+                                value={password2}
+                                name='password2'
+                                onChange={(e) => onChange(e)}
+                                // minLength='6'
+                              />
+
+                              <div className='button-box'>
+                                <button type='submit'>
                                   <span>Register</span>
                                 </button>
                               </div>
@@ -117,7 +197,14 @@ const LoginRegister = ({ location }) => {
 };
 
 LoginRegister.propTypes = {
-  location: PropTypes.object
+  location: PropTypes.object,
+  register: PropTypes.func.isRequired,
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
 };
 
-export default LoginRegister;
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { login, register })(LoginRegister);
