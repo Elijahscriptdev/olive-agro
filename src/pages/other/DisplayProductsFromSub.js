d410263877105ff7fe047988ea9564a2ffb0a658
Elijahscriptdev <elijahobominuru@gmail.com>
2021-03-05 16:59:04 +0100

    styled single product page

--- src/pages/other/DisplayProductsFromSub.js
+++ src/pages/other/DisplayProductsFromSub.js
@@ -2,8 +2,15 @@ import React, { Fragment, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { listProducts } from "../../redux/actions/productsActions";
+import PropTypes from "prop-types";
+import MetaTags from "react-meta-tags";
+import LayoutOne from "../../layouts/LayoutOne";
 
-const DisplayProductsFromSub = ({ location }) => {
+const DisplayProductsFromSub = ({
+  location,
+  sliderClassName,
+  spaceBottomClass,
+}) => {
   const productList = useSelector((state) => state.productList);
   const { products, loading, error } = productList;
   const dispatch = useDispatch();
@@ -19,30 +26,60 @@ const DisplayProductsFromSub = ({ location }) => {
   });
 
   return (
-    <div>
-      {loading ? (
-        <div>loading......</div>
-      ) : !matchedProductFromCat ? (
-        <div>No product added to this category. Please check back later</div>
-      ) : (
-        matchedProductFromCat.map((product, id) => {
-          return (
-            <div key={id} className='product-img'>
-              <Link to={`/product/${product._id}`}>
-                <img
-                  className='default-img mt-4'
-                  src={process.env.PUBLIC_URL + product.imageUrl}
-                  alt=''
-                />
-                <h4>{product.name}</h4>
-              </Link>
-              <h4>{product.category_name}</h4>
-            </div>
-          );
-        })
-      )}
-    </div>
+    <Fragment>
+      <MetaTags>
+        <title>Display All Products</title>
+        <meta name='Display Products' content='Display Products.' />
+      </MetaTags>
+      <LayoutOne headerTop='visible'>
+        <div className='container'>
+          <div className='row'>
+            {loading ? (
+              <div>loading......</div>
+            ) : !matchedProductFromCat ? (
+              <div>
+                No product added to this category. Please check back later
+              </div>
+            ) : (
+              matchedProductFromCat.map((product, id) => {
+                return (
+                  <div
+                    key={id}
+                    className={`col-xl-3 col-md-6 col-lg-4 col-sm-6 ${
+                      sliderClassName ? sliderClassName : ""
+                    }`}
+                  >
+                    <div
+                      className={`product-wrap ${
+                        spaceBottomClass ? spaceBottomClass : ""
+                      }`}
+                    >
+                      <div className='product-img'>
+                        <Link to={`/product/${product._id}`}>
+                          <img
+                            className='default-img mt-4'
+                            src={process.env.PUBLIC_URL + product.imageUrl}
+                            alt=''
+                          />
+                          <h4>{product.name}</h4>
+                        </Link>
+                        <h4>{product.category_name}</h4>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })
+            )}
+          </div>
+        </div>
+      </LayoutOne>
+    </Fragment>
   );
+};
+
+DisplayProductsFromSub.propTypes = {
+  spaceBottomClass: PropTypes.string,
+  wishlistItem: PropTypes.object,
 };
 
 export default DisplayProductsFromSub;
--- src/pages/other/SingleProduct.js
+++ src/pages/other/SingleProduct.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { listProducts } from "../../redux/actions/productsActions";
+import PropTypes from "prop-types";
+import MetaTags from "react-meta-tags";
+import LayoutOne from "../../layouts/LayoutOne";
 
-const SingleProduct = ({ location }) => {
+const SingleProduct = ({ location, sliderClassName, spaceBottomClass }) => {
   const { id } = useParams();
   const productList = useSelector((state) => state.productList);
   const { products, loading } = productList;
@@ -20,28 +23,68 @@ const SingleProduct = ({ location }) => {
   }, []);
 
   return (
-    <div>
-      {loading ? (
-        <div>loading......</div>
-      ) : (
-        matchedProduct.map((product) => {
-          return (
-            <div key={product._id} className='product-img'>
-              <img
-                className='default-img mt-4'
-                src={process.env.PUBLIC_URL + product.imageUrl}
-                alt=''
-              />
-              <h4>{product.name}</h4>
-              <p>₦{product.price_range}</p>
-              <p>Quantity: {product.quantity}</p>
-              <p>Category: {product.category_name}</p>
-            </div>
-          );
-        })
-      )}
-    </div>
+    <Fragment>
+      <MetaTags>
+        <title>Display All Products</title>
+        <meta name='Display Products' content='Display Products.' />
+      </MetaTags>
+      <LayoutOne headerTop='visible'>
+        <div className='container'>
+          <div className='row'>
+            {loading ? (
+              <div>loading......</div>
+            ) : (
+              matchedProduct.map((product) => {
+                return (
+                  <>
+                    <div
+                      key={id}
+                      className={`col-xl-5 col-md-6 col-lg-4 col-sm-6 increase-col-height ${
+                        sliderClassName ? sliderClassName : ""
+                      }`}
+                    >
+                      <div
+                        className={`product-wrap ${
+                          spaceBottomClass ? spaceBottomClass : ""
+                        }`}
+                      >
+                        <div key={product._id} className='product-img'>
+                          <img
+                            className='default-img mt-4'
+                            src={process.env.PUBLIC_URL + product.imageUrl}
+                            alt=''
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      className={`col-xl-6 col-md-6 col-lg-4 col-sm-6 mt-3 product-details `}
+                    >
+                      <div className='product-detail'>
+                        <h2>{product.name}</h2>
+                        <span className='available'>{product.status}</span>
+                        <p>₦{product.price_range}</p>
+                        <p>Quantity: {product.quantity}</p>
+                        <p>Category: {product.category_name}</p>
+                        <button className='product-detail-btn'>
+                          Contact Seller
+                        </button>
+                      </div>
+                    </div>
+                  </>
+                );
+              })
+            )}
+          </div>
+        </div>
+      </LayoutOne>
+    </Fragment>
   );
+};
+
+SingleProduct.propTypes = {
+  spaceBottomClass: PropTypes.string,
+  wishlistItem: PropTypes.object,
 };
 
 export default SingleProduct;
